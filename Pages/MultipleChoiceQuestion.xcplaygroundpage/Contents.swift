f276bb855104d9145940922ea42d4a826e3ef719
shankars <shankar.shinde@nitorinfotech.com>
2019-08-11 21:59:47 +0530

    GCD, closure and searching question

--- Pages/MultipleChoiceQuestion.xcplaygroundpage/Contents.swift
+++ Pages/MultipleChoiceQuestion.xcplaygroundpage/Contents.swift
@@ -62,3 +62,77 @@ let names3 = ["Simon", "River", "Book"]
 names3.forEach {
     print($0)
 }
+
+print("----Q12:")
+var cars = "benz";
+
+var clouserHolds = {  [cars]  in
+    let car = cars;
+    print("The value of car is \(car)");
+}
+
+func clouserHoldsF(){
+    //let car = cars;
+    print("the value of car is \(cars)");
+}
+cars = "audi";
+
+clouserHolds();
+clouserHoldsF();
+
+print("----Q12.1:")
+// Here both print Audi
+var cars12_1 = "benz";
+
+var clouserHolds12_1 = {
+    let car = cars12_1;
+    print("The value of car is \(car)");
+}
+
+func clouserHoldsF12_1(){
+    //let car = cars;
+    print("the value of car is \(cars12_1)");
+}
+cars12_1 = "audi";
+
+clouserHolds12_1();
+clouserHoldsF12_1();
+
+
+print("----Q13:")
+let queue = DispatchQueue.init(label: "raju");
+queue.async {
+    for i in 0 ... 10 {
+        print("Custom Queue: \(i)");
+    }
+}
+
+DispatchQueue.main.async {
+    for i in 0 ... 10 {
+        print("Main Queue:   \(i)");
+    }
+}
+
+print("----Q14: Count occurance of element in array")
+let array = [1,1,2,1,5,6]
+func makeGroup(list:[Int])-> [String:Int] {
+    var localGroup : [String:Int] = [:]
+    
+    for value in list {
+        if let valueFoundForKey = localGroup[String(value)] {
+            localGroup[String(value)] = valueFoundForKey + 1
+        }
+        else {
+            localGroup[String(value)] = 1
+        }
+    }
+    return localGroup
+}
+
+let group =  makeGroup(list: array)
+print(group[String(1)])
+print( group[String(5)])
+print( group[String(7)])
+
+
+
